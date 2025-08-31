72916085eb997a4e3c48c31e99bbce3255fe15d7
Milan <milan.milancen12345@gmail.com>
2025-08-31 18:27:30 +0200

    fix(ui):fixed mobile device viewport height

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { Skills } from "@/components/Skills";
 
 
 export default function Home() {
-  return  <div className=" w-screen max-h-screen  min-h-screen flex flex-col overflow-hidden bg-black">
+  return  <div className=" w-screen md:max-h-screen min-h-dvh md:min-h-screen flex flex-col overflow-hidden bg-black">
       <Navbar />
       <main className="flex flex-1  min-h-0 overflow-hidden md:px-[5%] py-[1%] px-[5%] md:gap-5 z-1">
         <div className="flex-3 min-h-0 w-full">
